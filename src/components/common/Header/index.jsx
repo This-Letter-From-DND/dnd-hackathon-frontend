3050b8987bc17708554718e9b7b43146a34da076
myungju <myungju030@gmail.com>
2024-05-18 05:20:42 +0900

    feat : header back icon 변경

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,12 +1,25 @@
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import { HeaderDoneStyled, HeaderStyled, HeaderTitleStyled } from './styles';
 
+import back from '@/assets/back.svg';
 export default function Header({ title, canGoBack, canDone }) {
   const router = useRouter();
 
   const HeaderLeftComponent = () => {
-    return <div onClick={router.back}>{canGoBack ? 'GoBack' : ''}</div>;
+    return (
+      <div onClick={router.back}>
+        {canGoBack ? (
+          <Image
+            src={back}
+            alt='back'
+          />
+        ) : (
+          ''
+        )}
+      </div>
+    );
   };
 
   const HeaderTitleComponent = () => {
