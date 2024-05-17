61a40fe91bc3585880a787b4e55166902d704308
myungju <myungju030@gmail.com>
2024-05-17 23:35:34 +0900

    feat : 디자인 css 파일 추가

---
+++ src/styles/reset.js
@@ -0,0 +1,50 @@
+export const theme = {
+  colors: {
+    main: {
+      primary: '#FFCA0C',
+      warning: '#FF372B',
+    },
+    neutral: {
+      900: '#212529',
+      800: '#343A40',
+      700: '#495057',
+      600: '#868E96',
+      500: '#ADB5BD',
+      400: '#CED4DA',
+      300: '#DEE2E6',
+      200: '#E9ECEF',
+      100: '#F1F3F5',
+      50: '#F8F9FA',
+    },
+    background: {
+      primary: '#F1F3F5',
+    },
+  },
+  font: {
+    fontSize: {
+      xsmall: '0.5rem',
+      small: '0.75rem',
+      medium: '1rem',
+      large: '1.25rem',
+    },
+    colors: {
+      white: '#fff',
+      black: '#000',
+    },
+    fontStyle: {
+      bold: 600,
+      medium: 400,
+    },
+  },
+  radius: {
+    small: '0.75rem',
+    medium: '1rem',
+    large: '1.125rem',
+    xlarge: '1.75rem',
+  },
+  layout: {
+    minWidth: {
+      mobile: '400px',
+    },
+  },
+};
