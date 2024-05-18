6f8bd562be1b6131e1fde2ac1cc08eaf5504e21a
myungju <myungju030@gmail.com>
2024-05-18 09:03:55 +0900

    fix : 내가 쓴 질문 페이지 수정

--- src/app/lists/user/page.jsx
+++ src/app/lists/user/page.jsx
@@ -8,13 +8,13 @@ import ai from '@/assets/aiai.svg';
 import user from '@/assets/user.svg';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
-import { getAllQuestionApi } from '@/services/question';
+import { getQuestionApi } from '@/services/question';
 export default function ListsUser() {
   const [list, setList] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const data = await getAllQuestionApi(1, 'recent', true);
+      const data = await getQuestionApi(1);
       setList(data);
     };
     getData();
@@ -27,6 +27,15 @@ export default function ListsUser() {
 
   const handleClickButton = () => {};
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}.${month}.${day}`;
+  }
+
   return (
     <Wrapper>
       <Header
@@ -38,7 +47,8 @@ export default function ListsUser() {
           <QuestionCardContainer key={index}>
             <QuestionCard>
               <TimeContainer>
-                <Time>2024.01.01</Time> <Button>후기작성</Button>
+                <Time>{formatDate(item.createdAt || '')}</Time>{' '}
+                <Button>후기작성</Button>
               </TimeContainer>
               <CardTop>
                 <CardTitle>{item.title}</CardTitle>
--- src/services/question.js
+++ src/services/question.js
@@ -9,7 +9,7 @@ export const getAllQuestionApi = async (id, sorter, isMine) => {
 };
 
 export const getQuestionApi = async (id) => {
-  return getFetch(`${API_URLS.question}?userId=${id}`);
+  return getFetch(`${API_URLS.question}/list/my?userId=${id}`);
 };
 
 export const postQuestionApi = async (data) => {
