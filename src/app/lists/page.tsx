31472200662feb6945e3558abbbd9934617cd40f
myungju <myungju030@gmail.com>
2024-05-21 20:57:45 +0900

    fix : 에러 수정

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -416,7 +416,7 @@ const Circle = styled.div`
   background-color: ${(props) => props.theme.colors.neutral[300]};
 `;
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
