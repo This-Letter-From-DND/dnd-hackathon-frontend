102492e2ca3f657c85da540bcbcc536551ee28d5
myungju <myungju030@gmail.com>
2024-06-02 23:55:24 +0900

    fix : Progressbar 에러 수정

--- src/components/question/ResultProgressBar/styles.tsx
+++ src/components/question/ResultProgressBar/styles.tsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
 export const ProgressBarContainer = styled.div`
-  width: 100%; /* 전체 너비 */
+  width: 100%;
   height: 40px;
   display: flex;
   flex-direction: row;
   align-items: center;
   border-color: ${(props) => props.theme.colors[300]};
   justify-content: space-between;
-  background-color: ${(props) => props.theme.colors[200]}; /* 배경색 */
-  border-radius: 8px; /* 테두리 둥글게 */
+  background-color: ${(props) => props.theme.colors[200]};
 `;
 
 export const ProgressBarSection = styled.div`
   width: 100%;
   height: 40px;
+  display: flex;
+  position: relative;
 `;
 
 interface ProgressBarDetailsProps {
@@ -25,19 +26,24 @@ interface ProgressBarDetailsProps {
 export const ProgressBarDetails = styled.div<ProgressBarDetailsProps>`
   display: flex;
   flex-direction: row;
-  justify-content: ${(props) => props.direction};
+  justify-content: ${(props) =>
+    props.direction === 'left' ? 'flex-start' : 'flex-end'};
   align-items: center;
   height: 100%;
   width: ${(props) => props.ratio};
   background-color: ${(props) => props.theme.colors.primary};
-
-  border-radius: 8px; /* 테두리 둥글게 */
+  position: absolute;
+  ${(props) => (props.direction === 'left' ? 'left: 0;' : 'right: 0;')}
+  border-radius: 8px;
 `;
 
 export const LabelWrap = styled.div<{ direction: string }>`
   display: flex;
   align-items: center;
-  justify-content: ${(props) => props.direction};
+  justify-content: ${(props) =>
+    props.direction === 'left' ? 'flex-start' : 'flex-end'};
+  margin: ${(props) =>
+    props.direction === 'left' ? '0 0 0 10px' : '0 10px 0 0'};
 
   > :first-child {
     margin-right: 0.5rem;
@@ -52,11 +58,11 @@ export const Label = styled.div`
 export const LabelLeft = styled(Label)<{ $small?: boolean }>`
   left: 0;
   color: ${(props) => (props.$small ? props.theme.colors[500] : '')};
-  margin-left: 10px; /* 좌측 라벨 패딩 */
+  position: absolute;
 `;
 
 export const LabelRight = styled(Label)<{ $small?: boolean }>`
-  right: 0; /* 오른쪽에서 시작 */
-  padding-right: 10px; /* 우측 패딩 */
+  right: 0;
   color: ${(props) => (props.$small ? props.theme.colors[500] : '')};
+  position: absolute;
 `;
